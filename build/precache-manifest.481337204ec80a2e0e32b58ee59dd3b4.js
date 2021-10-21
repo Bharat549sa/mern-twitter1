0da76fd80db4a71090552c2e55a11321150730b9
bharat sharma <Bharat549sa@gmail.com>
2021-10-21 16:58:12 -0400

    sdsd

---
+++ build/precache-manifest.481337204ec80a2e0e32b58ee59dd3b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19fbc25faa0fb99f5620",
+    "url": "/static/css/main.d6de7a37.chunk.css"
+  },
+  {
+    "revision": "19fbc25faa0fb99f5620",
+    "url": "/static/js/main.19fbc25f.chunk.js"
+  },
+  {
+    "revision": "58be54cd78b1b6d24b17",
+    "url": "/static/js/1.58be54cd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2909885838d8bd23da35e483237700a0",
+    "url": "/index.html"
+  }
+];
